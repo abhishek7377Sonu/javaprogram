d6509ecb082b20f3a74581dbabcda336f4b5fa43
abhishek7377Sonu <abhishek73770@gmail.com>
2024-12-22 17:48:37 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,21 @@
+package Logical;
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		int n=153, sum=0, temp,rem;
+		temp=n;
+		
+		while(n>0) {
+			rem=n%10;
+			sum=sum+(rem*rem*rem);
+			n=n/10;
+		}
+		if(temp==sum) 
+			System.out.println(temp + "  is a armstrong number");
+		
+		else 
+			System.out.println(temp+" Not a armstrong number");
+		
+	}
+}
